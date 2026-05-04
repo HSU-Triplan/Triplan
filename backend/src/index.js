1435d641aa52c9b5af285ff40047ac0697acf303
jaeminKimPea <kg8480@gmail.com>
2026-05-04 14:39:23 +0900

    init

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,21 +4,20 @@ const cors = require('cors');
 require('dotenv').config();
 
 
-
-const authRouter = require('./routes/auth');	const authRouter = require('./routes/auth');
+const authRouter = require('./routes/auth');
 
 
 const app = express();	const app = express();
 
 app.get('/', (req, res) => {
-const postsRouter = require('./routes/posts');	const postsRouter = require('./routes/posts');
-app.use('/posts', postsRouter);	app.use('/posts', postsRouter);
+const postsRouter = require('./routes/posts');	
+app.use('/posts', postsRouter);
 
 
 const usersRouter = require('./routes/users');	
 app.use('/users', usersRouter);	
 
-const PORT = process.env.PORT || 3000;	const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {	app.listen(PORT, () => {
-  console.log(`서버 실행 중: http://localhost:${PORT}`);	  console.log(`서버 실행 중: http://localhost:${PORT}`);
+  console.log(`서버 실행 중: http://localhost:${PORT}`);
 });
