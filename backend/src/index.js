351cf753a99528069a5069fb2075704b14c7f740
jaeminKimPea <kg8480@gmail.com>
2026-05-04 14:37:21 +0900

    init

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 
-const express = require('express');	const express = require('express');
-const cors = require('cors');	const cors = require('cors');
-require('dotenv').config();	require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
 
 
