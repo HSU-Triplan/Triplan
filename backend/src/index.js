26203eae30bf8b434de9a03fcc36fde4d28091b0
kdh <ihel@naver.com>
2026-06-02 16:45:02 +0900

    fix/alarm-detail

--- backend/src/index.js
+++ backend/src/index.js
@@ -66,7 +66,7 @@ io.on('connection', (socket) => {
     try{
         let {data , error} = await supabase
             .from('chat_members')
-            .select('user_id,users(fcm_token)')
+            .select('user_id,users(fcm_token,profile_image,name,nickname)')
             .eq('chat_room_id',roomId);
         console.log("data : "+ JSON.stringify(data))
         for(let i=0 ; i<data.length ; i++){
@@ -80,8 +80,9 @@ io.on('connection', (socket) => {
                 token : data[i].users.fcm_token,
 
                 notification : {
-                    title : '메세지 알람',
-                    body : '채팅방에 새 메세지가 전송되었습니다.'
+                    title : data[i].users.nickname || data[i].users.name,
+                    body : '채팅방에 새 메세지가 전송되었습니다.',
+                    imageUrl : data[i].users.profile_image
                 },
 
                 android: {
